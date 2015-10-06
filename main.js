2377ccd092b236e796254f8c6e426ae12f2d55d2
Student <student@RU-019.local>
2015-10-06 11:57:49 -0600

    first done

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-// var addNumbers = function(string){
-
-// }
-
-var longestWord = function(words){
-
-	var str = words.split( ' ')
-	var longest = 0
-	var word = null
-
-	for (var i=0; i < str.length; i++ ){
-		if (longest < str[i].length){
-			longest = str[i].length
-			word = str[i]
-		}
+var addNumbers = function(string){
+	var words = string.split(' ')
+	var a = ''
+	var b = 0
+	for (var i = 0; i <words.length; i++){
+		a = parseInt(words[i])
+		b += a
 	}
-
-
-
-	return (word)
+	return b
 }
-console.log(longestWord('Why not this one?'))
+
+console.log(addNumbers('12Hello 3World!'))
+
+// var longestWord = function(words){
+
+// 	var str = words.split( ' ')
+// 	var longest = 0
+// 	var word = null
+
+// 	for (var i=0; i < str.length; i++ ){
+// 		if (longest < str[i].length){
+// 			longest = str[i].length
+// 			word = str[i]
+// 		}
+// 	}
+
+
+
+// 	return (word)
+// }
+// console.log(longestWord('Why not this one?'))
