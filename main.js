55f8ec4670507be939d04ce7bff02eee9129b4c0
Student <student@RU-019.local>
2015-10-07 08:48:24 -0600

    far as I got

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var addNumbers = function(string){
-	var words = string.split(' ')
-	var a = ''
-	var b = 0
-	for (var i = 0; i <words.length; i++){
-		a = parseInt(words[i])
-		b += a
-	}
-	return b
-}
+// var addNumbers = function(string){
+// 	var words = string.split(' ')
+// 	var a = ''
+// 	var b = 0
+// 	for (var i = 0; i <words.length; i++){
+// 		a = parseInt(words[i])
+// 		b += a
+// 	}
+// 	return b
+// }
 
-console.log(addNumbers('12Hello 3World!'))
+// console.log(addNumbers('12Hello 3World!'))
 
 // var longestWord = function(words){
 
@@ -29,3 +29,16 @@ console.log(addNumbers('12Hello 3World!'))
 // 	return (word)
 // }
 // console.log(longestWord('Why not this one?'))
+
+var averageStringNumbers = function(string){
+	 	var words = string.split('')
+	 	// var strings = words.join('')
+	 	console.log(strings)
+		var a = ''
+		var b = 0
+		for (var i = 0; i <words.length; i++){
+			a = parseInt
+		}
+	return b/strings.length
+}
+console.log(averageStringNumbers('bl2ah bla4h bla7h bl1ah'))
